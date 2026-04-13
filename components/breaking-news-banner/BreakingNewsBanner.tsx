d26e6fcc38aff4b7da4a701b66ae010b05704712
andreandradearke <aandrade@arkesystems.com>
2026-04-13 08:39:55 -0300

    Add props to BreakingNewsBanner and changes home.tsx to Home.tsx

--- components/breaking-news-banner/BreakingNewsBanner.tsx
+++ components/breaking-news-banner/BreakingNewsBanner.tsx
@@ -1,10 +1,14 @@
-export default function BreakingNewsBanner() {
+interface BreakingNewsBannerProps {
+    headline: string;
+    featuredText: string;
+}
+export default function BreakingNewsBanner({ headline, featuredText }: BreakingNewsBannerProps) {
     return (
-        <div className="md:py-4 md:px-24 py-2 px-2 bg-black text-white">
+        <div className="md:py-4 md:px-24 p-2 bg-black text-white">
             <p className="flex gap-2 leading-tight text-sm items-center">
                 <span>&#9888;</span>
-                <span className="bg-white text-black px-2 py-1 rounded font-bold leading-none content-center text-sm">Breaking</span>
-                Vercel CDN Noe Collapses Over 3M Requests Per Day
+                <span className="bg-white text-black px-2 py-1 rounded font-bold leading-none content-center text-sm">{featuredText}</span>
+                {headline}
             </p>
         </div>
     );
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,15 +1,9 @@
 import BreakingNewsBanner from "@/components/breaking-news-banner/BreakingNewsBanner";
+
 export default function Home() {
     return (
         <>
-            <BreakingNewsBanner />
-            <div className="container mx-auto py-8 px-4">
-                <h1 className="text-3xl font-bold mb-6">Welcome to Vercel Daily News</h1>
-                <p className="text-lg mb-4">
-                    Stay updated with the latest news and articles from the tech world.
-                </p>
-            </div>
-
+            <BreakingNewsBanner headline="Vercel CDN Node Collapses Over 3M Requests Per Day" featuredText="Breaking" />
         </>
     );
 }
